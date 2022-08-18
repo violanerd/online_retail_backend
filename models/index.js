2b24af0ce776008cdd14ab7d95d10b32211fa428
violanerd <madalyne.cross@gmail.com>
2022-08-18 14:56:57 -0500

    tests in sql sheell

--- models/index.js
+++ models/index.js
@@ -15,13 +15,13 @@ Category.hasMany(Product, {
 // Products belongToMany Tags (through ProductTag)
 Product.belongsToMany(Tag, {
   through: ProductTag,
-  as: 'product_tag',
+  as: 'products',
   foreignKey: 'product_id'
 })
 // Tags belongToMany Products (through ProductTag)
 Tag.belongsToMany(Product, {
   through: ProductTag,
-  as: 'product_tag',
+  as: 'tags',
   foreignKey: 'tag_id'
 })
 
