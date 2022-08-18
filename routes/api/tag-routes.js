dcfed0caff02999854e25d9edc323f956d8216a0
violanerd <madalyne.cross@gmail.com>
2022-08-18 14:36:20 -0500

    added models and associations

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -3,24 +3,29 @@ const { Tag, Product, ProductTag } = require('../../models');
 
 // The `/api/tags` endpoint
 
+// GET /api/tags
 router.get('/', (req, res) => {
   // find all tags
   // be sure to include its associated Product data
 });
 
+//GET /api/tags/1
 router.get('/:id', (req, res) => {
   // find a single tag by its `id`
   // be sure to include its associated Product data
 });
 
+//POST /api/tags
 router.post('/', (req, res) => {
   // create a new tag
 });
 
+//PUT /api/tags/1
 router.put('/:id', (req, res) => {
   // update a tag's name by its `id` value
 });
 
+//DELETE /api/tags/1
 router.delete('/:id', (req, res) => {
   // delete on tag by its `id` value
 });
